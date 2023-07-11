68afeceef56628c402f2d56aeffed81794c1b8af
Yun <3430yun>
2023-07-11 20:16:47 +0800

    Initial Commit

---
+++ smtp/smtpApp.swift
@@ -0,0 +1,17 @@
+//
+//  smtpApp.swift
+//  smtp
+//
+//  Created by 呂沄 on 2023/7/11.
+//
+
+import SwiftUI
+
+@main
+struct smtpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
